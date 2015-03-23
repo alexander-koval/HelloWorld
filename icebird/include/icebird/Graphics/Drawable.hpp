c790cb33047d260ccca55c44f49d884ce4fd83c8
Alexander Koval <a.koval@socialquantum.ru>
2015-03-23 20:35:56 +0600

    Drawable interface

---
+++ icebird/include/icebird/Graphics/Drawable.hpp
@@ -0,0 +1,18 @@
+#ifndef DRAWABLE_HPP
+#define DRAWABLE_HPP
+
+
+class Drawable {
+public:
+
+    virtual ~Drawable() {}
+
+protected:
+
+//    friend class RenderTarget;
+
+    virtual void draw(RenderTarget& target, RenderStates states) const = 0;
+};
+
+
+#endif // DRAWABLE_HPP
